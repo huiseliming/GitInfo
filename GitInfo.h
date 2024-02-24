f7eadf701ce76044a6e388f440dacb99a90af4d6
huiseliming <huiseliming@qq.com>
2024-02-24 16:09:08 +0800

    Update GitInfo.h

--- GitInfo.h
+++ GitInfo.h
@@ -5,3 +5,6 @@ GITINFO_API bool GitIsDirty();
 GITINFO_API const char *GitCommitSHA1();
 GITINFO_API const char *GitCommitDate();
 GITINFO_API const char *GitDescribe();
+GITINFO_API const char *GitTagVersion();
+GITINFO_API const char *GitPatchVerison();
+GITINFO_API const char *GitBuildVersion();
