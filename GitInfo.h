a3d372d202072d30c7637cb9e35fa898184b26cb
huiseliming <huiseliming@qq.com>
2024-02-28 16:49:49 +0800

    Update GitInfo.h

--- GitInfo.h
+++ GitInfo.h
@@ -1,10 +1,15 @@
 #pragma once
-#include "GitInfo.api.h"
 
-GITINFO_API bool GitIsDirty();
-GITINFO_API const char *GitCommitSHA1();
-GITINFO_API const char *GitCommitDate();
-GITINFO_API const char *GitDescribe();
-GITINFO_API const char *GitTagVersion();
-GITINFO_API const char *GitPatchVerison();
-GITINFO_API const char *GitBuildVersion();
+bool GitIsPopulated();
+bool GitAnyUncommittedChanges();
+const char* GitAuthorName();
+const char* GitAuthorEmail();
+const char* GitCommitSHA1();
+const char* GitCommitDate();
+const char* GitCommitSubject();
+const char* GitCommitBody();
+const char* GitDescribe();
+const char* GitBranch();
+const char* GitTagVersion();
+const char* GitPatchVersion();
+const char* GitBuildVersion();
